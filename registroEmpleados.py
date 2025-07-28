69829139cf4f935d597406dea36e62820047db97
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-28 11:11:01 -0600

    arreglando algunos errores en el codigo

--- registroEmpleados.py
+++ registroEmpleados.py
@@ -10,18 +10,18 @@ class empleado:
 
 
 class evaluacion:
-    def __init__(self, puntualidad, equipo, productividad, observacion):
+    def __init__(self, puntualidad, equipo, productividad,observaciones):
         self.puntualidad = puntualidad
         self.equipo = equipo
         self.productividad = productividad
-        self.observacion = observacion
+        self.observaciones = observaciones
 
     def calcular_promedio(self):
         return (self.puntualidad + self.equipo + self.productividad) / 3
 
     def mostrar_info(self):
         print(f"Puntualidad: {self.puntualidad}, Equipo: {self.equipo}, Productividad: {self.productividad}")
-        print(f"Observación: {self.observacion}")
+        print(f"Observaciones: {self.observaciones}")
         print(f"Promedio: {self.calcular_promedio():.2f}")
 
 
@@ -53,6 +53,7 @@ while opcion != 5:
         print("Registro Empleado")
         cantidad = int(input("Cuantos empleados desea registrar?: "))
         for c in range(cantidad):
+            print(f"Empleado #{c+1}")
             codigo = input("Codigo: ")
             nombre = input("Nombre: ")
             departamento = input("Departamento: ")
@@ -61,12 +62,12 @@ while opcion != 5:
             puntualidad = int(input("Puntuacion en puntualidad (0-10): "))
             productividad = int(input("Puntuacion en Productividad (0-10): "))
             equipo = int(input("Puntuacion en Equipo (0-10): "))
-            observacion = input("Observacion: ")
+            observaciones = int(input("Observaciones: "))
             telefono = input("Telefono: ")
             correo = input("Correo: ")
 
             empleado1 = empleado(codigo, nombre, departamento, antiguedad)
-            evavaluacion1 = evaluacion(puntualidad, equipo, productividad, observacion)
+            evavaluacion1 = evaluacion(puntualidad, equipo, productividad,observaciones)
             contacto1 = contacto(correo, telefono)
 
             empleados[codigo] = {
@@ -96,7 +97,14 @@ while opcion != 5:
     elif opcion == 4:
         codigo = input("Ingrese el código del empleado: ")
         if codigo in empleados:
-            empleados[codigo]["evaluacion"].mostrar_info()
+            for emp1 in empleados.values():
+                emp1["empleado"].mostrar_info()
+                empleados[codigo]["evaluacion"].mostrar_info()
+                promedio = empleados[codigo]["evaluacion"].calcular_promedio()
+                if promedio >= 7:
+                    print("estado : satisfactoria")
+                else:
+                    print("estado : mejorar")
         else:
             print("Empleado no encontrado.")
     elif opcion == 5:
